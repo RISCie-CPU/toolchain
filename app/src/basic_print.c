a5a21e24c485d8d8c9b74ecc14060759196193ed
wykys <vvykys@gmail.com>
2022-03-01 21:43:15 +0100

    Accelerated basic print

--- app/src/basic_print.c
+++ app/src/basic_print.c
@@ -11,6 +11,25 @@ void set_cursor(uint32_t x, uint32_t y)
     cursor.y = y;
 }
 
+const uint32_t CODE_4B_TO_32B[16] = {
+    0x00000000, // 0x00
+    0x000000FF, // 0x01
+    0x0000FF00, // 0x02
+    0x0000FFFF, // 0x03
+    0x00FF0000, // 0x04
+    0x00FF00FF, // 0x05
+    0x00FFFF00, // 0x06
+    0x00FFFFFF, // 0x07
+    0xFF000000, // 0x08
+    0xFF0000FF, // 0x09
+    0xFF00FF00, // 0x0A
+    0xFF00FFFF, // 0x0B
+    0xFFFF0000, // 0x0C
+    0xFFFF00FF, // 0x0D
+    0xFFFFFF00, // 0x0E
+    0xFFFFFFFF  // 0x0F
+};
+
 void basic_print(char *str)
 {
     for (uint32_t i = 0; str[i]; i++)
@@ -30,17 +49,22 @@ void basic_print(char *str)
             continue;
         }
 
-        for (int32_t y = 0; y < CHAR_Y_SIZE; y++)
+        for (uint32_t y = 0; y < CHAR_Y_SIZE; y++)
         {
-            for (int32_t x = 0; x < CHAR_X_SIZE; x++)
-            {
-                uint32_t address = (x + (cursor.x * CHAR_X_SIZE)) + ((y + (cursor.y * CHAR_Y_SIZE)) * SCREEN_PX_WIDTH);
-                uint8_t value    = (
-                    ASCII[(uint8_t) str[i]][y] & (1 << x)
-                  ) ? 0xFF : 0x00;
-                vram_write_8b(address, value);
-            }
+            uint32_t address;
+            uint32_t value;
+
+            uint8_t char_line = ASCII[(uint8_t) str[i]][y];
+
+            address = (0 + (cursor.x * CHAR_X_SIZE)) + ((y + (cursor.y * CHAR_Y_SIZE)) * SCREEN_PX_WIDTH);
+            value   = CODE_4B_TO_32B[char_line & 0x0F];
+            vram_write_32b(address, value);
+
+            address = (4 + (cursor.x * CHAR_X_SIZE)) + ((y + (cursor.y * CHAR_Y_SIZE)) * SCREEN_PX_WIDTH);
+            value   = CODE_4B_TO_32B[char_line >> 4];
+            vram_write_32b(address, value);
         }
+
         if (++cursor.x > SCREEN_CHAR_WIDTH)
         {
             cursor.x = 0;
